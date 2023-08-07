fcb4d0e2c5a8fa67909ac602b5011a9cdcfe2a26
Konstantin kokadevops <kokadevops@gmail.com>
2023-08-07 19:30:00 +0300

    add features, services, utils

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,30 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from './ui/Home'
+import Menu from './features/menu/Menu'
+import CreateOrder from './features/order/CreateOrder'
+import Order from './features/order/Order'
+import Cart from './features/cart/Cart'
+
+const router=createBrowserRouter([
+    {path:'/',
+    element: <Home/>},
+
+  {  path:'/menu',
+    element: <Menu/>},
+
+  {  path:'/cart',
+    element: <Cart/>},
+
+  {  path:'/order/new',
+    element: <CreateOrder/>},
+
+  {  path:'/order/:orderId',
+    element: <Order/>}
+])
+
+
 const App = () => {
-    return <div>App</div>;
+    return <RouterProvider router={router}/>
 };
 export default App;
---
+++ src/ui/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div>
+      <h1>
+        The best pizza of the world.
+        <br />
+        Special edition
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
